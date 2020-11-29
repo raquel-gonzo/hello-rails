81c364e2f4ec14a6c5db09d595c6c4a00fa0c0f2
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-28 21:31:00 -0600

    made validation for creating a new article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,18 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
-    def new 
+    def new # creating a new instance variable called '@article'
+        @article = Article.new 
     end
 
     def create
         @article = Article.new(article_params) # initialize model. Article Params? 
 
-        @article.save # saves model to db. returns a boolean.
-        redirect_to @article
+        if @article.save    # returns boolean
+            redirect_to @article
+        else
+            render 'new' # @article object is passed back to the 'new' template 
+        end
     end
 
     private # defining the params is done in its own method so that other actions can use the same controller.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+    validates :title, presence: true, length: { minimum: 5 }
+    # each title will have a title that is at least 5 characters long.
+    
 end
