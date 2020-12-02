11636645d71429861253232f2e53c00f8c850f73
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-12-01 22:52:31 -0600

    added delete link to comment controller, added destroy action to comment controller, added dependent option to article model.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article) # redirects user back to original article. calls the 'show' action of the ArticlesController --> renders show.html.erb template.
     end
 
+    def destroy # finds article you're looking at, locates the comment within the article.comments collection, removes it from db and sends us back to the article. 
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[ :id ])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end 
+
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
