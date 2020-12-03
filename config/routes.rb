f619e1ffeb97cb88fae3580c4e5ddba38fb85d69
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-12-02 23:45:32 -0600

    added background color and basic styling. edited article index for main show view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcom/index'
+  get 'articles/index'
 
   resources :articles do # declares a REST source. 
     resources :comments
   end
-  root 'welcom#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
